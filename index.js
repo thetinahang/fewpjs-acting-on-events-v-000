4c86b79a97f20494de6b4ae9184ad1b5f19360c6
Tina Hang <mstinahang@gmail.com>
2020-01-07 04:41:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
   let left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left = 1}px`;
+    dodger.style.left = `${left - 1}px`;
   }
 }
 
